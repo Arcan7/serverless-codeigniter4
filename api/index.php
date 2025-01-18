d34a66bae9879faee2ac115cf729b0369b784619
Arcan7 <aerabenandrasana@gmail.com>
2025-01-18 11:48:51 +0300

    Refactor Vercel PHP entry point in index.php for improved application bootstrapping and change cache handler to 'array' in Cache.php for better performance.

--- api/index.php
+++ api/index.php
@@ -1,5 +1,29 @@
 <?php
-// here is the serverless function entry point for deployment on vercel
+// Définir le répertoire racine
+define('FCPATH', dirname(__DIR__) . '/public/');
+chdir(FCPATH);
 
-require __DIR__.'/../public/index.php';
+// Récupérer le chemin du front controller
+$pathsPath = FCPATH . '../app/Config/Paths.php';
+
+// Charger la classe Paths
+require $pathsPath;
+$paths = new Config\Paths();
+
+// Vérifier si l'application est installée
+if (!is_file($paths->systemDirectory . '/Boot.php')) {
+    exit('Application not installed! Please follow instructions in README.md');
+}
+
+// Configuration pour Vercel
+$paths->setSystemDirectory(dirname(__DIR__) . '/system');
+$paths->setApplicationDirectory(dirname(__DIR__) . '/app');
+$paths->setWritableDirectory('/tmp'); // Utiliser le dossier temporaire de Vercel
+
+// Load framework bootstrap file
+require $paths->systemDirectory . '/Boot.php';
+
+// Lancer l'application
+$app = new CodeIgniter\Boot\Boot();
+exit($app->bootWeb($paths));
 ?>
