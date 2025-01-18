8cc742d57c115b26250410364d2a63cbe3d26a63
Arcan7 <aerabenandrasana@gmail.com>
2025-01-18 11:57:01 +0300

    Implement PHP version check in index.php for CodeIgniter compatibility, ensuring the application runs on PHP 8.1 or higher. Update paths loading mechanism for improved clarity and streamline the framework bootstrapping process. Adjust Vercel configuration for better handling of server arguments.

--- api/index.php
+++ api/index.php
@@ -7,17 +7,22 @@
 // Path to the front controller
 define('FCPATH', dirname(__DIR__) . '/public/');
 
-// Path to the project root directory
-define('ROOTPATH', dirname(__DIR__) . DIRECTORY_SEPARATOR);
-
-// Path to the system directory
-define('SYSTEMPATH', ROOTPATH . 'system' . DIRECTORY_SEPARATOR);
-
-// Path to the writable directory which will be using /tmp on Vercel
-define('WRITEPATH', '/tmp/');
-
-// The path to the "application" directory
-define('APPPATH', ROOTPATH . 'app' . DIRECTORY_SEPARATOR);
+/*
+ *---------------------------------------------------------------
+ * CHECK PHP VERSION
+ *---------------------------------------------------------------
+ */
+$minPhpVersion = '8.1';
+if (version_compare(PHP_VERSION, $minPhpVersion, '<')) {
+    $message = sprintf(
+        'Your PHP version must be %s or higher to run CodeIgniter. Current version: %s',
+        $minPhpVersion,
+        PHP_VERSION
+    );
+    header('HTTP/1.1 503 Service Unavailable.', true, 503);
+    echo $message;
+    exit(1);
+}
 
 // Ensure the current directory is pointing to the front controller's directory
 chdir(FCPATH);
@@ -30,27 +35,19 @@ chdir(FCPATH);
 
 // LOAD OUR PATHS CONFIG FILE
 require FCPATH . '../app/Config/Paths.php';
+
 $paths = new Config\Paths();
 
-// LOAD THE FRAMEWORK BOOT FILE
-require SYSTEMPATH . 'Boot.php';
+// Location of the framework bootstrap file.
+require rtrim($paths->systemDirectory, '\\/ ') . DIRECTORY_SEPARATOR . 'Boot.php';
 
-// Define environment
+// Define ENVIRONMENT
 if (!defined('ENVIRONMENT')) {
     define('ENVIRONMENT', $_ENV['CI_ENVIRONMENT'] ?? 'production');
 }
 
-// Temporarily switch to system directory
-chdir(SYSTEMPATH);
-
-// Load the framework constants
-require SYSTEMPATH . 'Config/Constants.php';
-
-// Return to the project root
-chdir(ROOTPATH);
-
-// Modifier la configuration du cache pour Vercel
-\Config\Services::cache()->handler = 'array';
-\Config\Services::cache()->backupHandler = 'dummy';
+// Configure for Vercel
+$_SERVER['argv'] = [];
+$_SERVER['argc'] = 0;
 
 exit(CodeIgniter\Boot::bootWeb($paths));
